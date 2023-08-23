bc34ac754566f65b573e45c60de902749c75f3e7
Blaine Curren <blaine.curren@gmail.com>
2023-08-23 06:40:31 -0500

    completed temp converter

--- class13-materials/class13-materials/temperature-converter/js/main.js
+++ class13-materials/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,11 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+document.querySelector("#yell").addEventListener("click", convert);
+
+function convert() {
+  let celsius = document.querySelector("#sel").value;
+
+  celsius = (celsius * 9) / 5 + 32;
+
+  document.querySelector("#placeToYell").innerText = celsius;
+}
