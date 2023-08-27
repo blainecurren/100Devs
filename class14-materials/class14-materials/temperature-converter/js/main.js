5e458a84691c517bebabc0bd7f19afed0f966946
Blaine Curren <blaine.curren@gmail.com>
2023-08-27 12:00:42 -0500

    finished temp converter

--- class14-materials/class14-materials/temperature-converter/js/main.js
+++ class14-materials/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// get temperature in celsius with input
+document.querySelector("#yell").addEventListener("click", convert);
+
+function convert() {
+  console.log("Converting");
+  let temp = document.querySelector("#firstName").value;
+  // convert temperature to fahrenheit
+  temp = (temp * 9) / 5 + 32;
+  // display temperature in fahrenheit
+  document.querySelector("#placeToYell").innerText = temp;
+}
