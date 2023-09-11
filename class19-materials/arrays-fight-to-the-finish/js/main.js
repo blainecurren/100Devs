b9d0aa18d778935d39c8c67c90dd8886e002aba4
Blaine Curren <blaine.curren@gmail.com>
2023-09-11 05:51:38 -0500

    read JSway ch. 6

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,12 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+let movies = ["fight", 'win', 'worldwide']
 
+for (let i = 0; i < movies.length; i++) {
+    document.querySelector('h2').innerText = movies[i]
+}
+//Create an array of numbers. Loop through the array add three to each number and replace the old number.
+let nums = [10, 20, 30]
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
+nums[0] = nums[0] + 3; // updates and reassigns  index 0 with 13
 
 //Find the average of all the numbers from question three
