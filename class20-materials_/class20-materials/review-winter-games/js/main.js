9dcb37e73b8ca4fbf5d80300ac347fa016efaf06
Blaine Curren <blaine.curren@gmail.com>
2023-09-17 22:25:29 -0500

    finished winter-games

--- class20-materials_/class20-materials/review-winter-games/js/main.js
+++ class20-materials_/class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function evenNumber(nums) {
+  let evens = [];
+
+  nums.forEach((n) => {
+    if (n % 2 === 0) {
+      evens.push(n);
+    }
+  });
+
+  return evens;
+}
