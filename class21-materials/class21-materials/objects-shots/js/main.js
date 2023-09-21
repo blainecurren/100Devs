64a9a4d61bf896949975f7e318587bd0b4058e9d
Blaine Curren <blaine.curren@gmail.com>
2023-09-20 22:04:54 -0500

    started objects-shots

--- class21-materials/class21-materials/objects-shots/js/main.js
+++ class21-materials/class21-materials/objects-shots/js/main.js
@@ -1,1 +1,21 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+const button = document.querySelector('buttons');
+button.addEventListener('click', getCocktail)
+
+
+function cocktail(name, image, instructions) {
+    this.name = name;
+    this.image = image;
+    this.instructions = instructions;
+}
+
+cocktail.prototype.createElement = function() {
+    const parentElement = document.getElementById("parent-container");
+    
+}
+
+function getCocktail() {
+  document.querySelector("#input").value`${cocktail.name}`;
+}
+
+function displayCocktail() {}
