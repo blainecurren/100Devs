ac5bbb776f535be3a1d67b985b561fcbe041fdbf
Blaine Curren <blaine.curren@gmail.com>
2023-10-02 09:35:21 -0500

    added class 23 materials

--- class23-materials/class23-materials/space-review/js/main.js
+++ class23-materials/class23-materials/space-review/js/main.js
@@ -1,18 +1,9 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
-let nums = [42, 19, 77, 33, 57];
-
-let sum = nums.reduce((acc, c) => acc + c, 0); // acc = accumulator c = current value 0 = accumulator starting at 0
-
-
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
-function arr (arr) {
-
-}
-
 
 //Create a function that takes string
 //Print the reverse of that string to the console
