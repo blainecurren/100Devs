e3ccbb3bcef8598a8c6b82c9b019824f8185a398
Blaine Curren <blaine.curren@gmail.com>
2023-10-14 20:03:17 -0500

    push for mac

--- class26-materials/class26-materials/objects-shots/js/main.js
+++ class26-materials/class26-materials/objects-shots/js/main.js
@@ -13,15 +13,15 @@ function Cocktail(name, image, instructions) {
 const button = document.querySelector("button");
 button.addEventListener("click", onClick);
 
-// TODO: Look into prototypes
-Cocktail.prototype.createElement = function () {
-  const parentElement = document.getElementById("parent-container");
+// TODO: Look into prototypes: Every object in JavaScript has a built-in property, which is called its prototype. The prototype is itself an object, so the prototype will have its own prototype, making what's called a prototype chain. The chain ends when we reach a prototype that has null for its own prototype.
+Cocktail.prototype.createElement = function () { // <- This is linked to the cocktail object
+  const parentElement = document.getElementById("parent-container"); // Adds parent container to hold all child elements that follow
   const container = document.createElement("div");
   const drinkName = document.createElement("h2");
   const drinkImage = document.createElement("img");
   const drinkInstructions = document.createElement("p");
 
-  drinkName.textContent = this.name;
+  drinkName.textContent = this.name; // Set drinkName's text content to the cocktail name property
   drinkImage.src = this.image;
   drinkInstructions.textContent = this.instructions;
   parentElement.appendChild(container);
@@ -31,7 +31,7 @@ Cocktail.prototype.createElement = function () {
 };
 
 function onClick() {
-  var name = document.querySelector("input").value;
+  var name = document.querySelector("input").value; // sets var name as the user input
   const cocktailURL = url + name;
   console.log(cocktailURL);
 
@@ -39,16 +39,16 @@ function onClick() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      const cocktailObjects = [];
-      const drinks = data.drinks;
-      for (let i = 0; i < drinks.length; i++) {
-        const cocktail = drinks[i];
-        const cocktailObject = new Cocktail(
+      const cocktailObjects = []; // creating an empty array to store data received by API call
+      const drinks = data.drinks; // sets data.drinks from the api as a const to be referenced later
+      for (let i = 0; i < drinks.length; i++) { // Loops through array to get new drinks
+        const cocktail = drinks[i]; // sets cocktail as the drink incrementor
+        const cocktailObject = new Cocktail( //creates a new object
           cocktail.strDrink,
           cocktail.strDrinkThumb,
           cocktail.strInstructions
         );
-        cocktailObjects.push(cocktailObject);
+        cocktailObjects.push(cocktailObject); 
         cocktailObject.createElement();
       }
       console.log(cocktailObjects);
