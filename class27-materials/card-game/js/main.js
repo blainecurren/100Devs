7d2bd37f6011aeedb56682c8257a682c0a07712d
Blaine Curren <blaine.curren@gmail.com>
2023-10-18 17:53:27 -0500

    updated card game

--- class27-materials/card-game/js/main.js
+++ class27-materials/card-game/js/main.js
@@ -1,5 +1,27 @@
-let deckId = ''
+let deckId = "";
 
-document.querySelector("button").addEventListener("click", dealTwo);
+fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    deckId = data.deck_id;
+  })
+  .catch((err) => {
+    console.log(`error${err}`);
+  });
 
-function dealTwo() {}
+document.querySelector("button").addEventListener("click", drawTwo);
+
+function drawTwo() {
+  const url = `https://www.decksofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    })
+    .catch((err) => {
+      console.log(`error${err}`);
+    });
+}
