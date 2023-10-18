12c03b6ac11c3072a9977f098997fb0df08d74db
Blaine Curren <blaine.curren@gmail.com>
2023-10-18 06:44:38 -0500

    added netflixshow class constructor

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,17 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+class NetflixShow {
+  constructor(name, genre, watched, stars) {
+    this.name = name;
+    this.genre = genre;
+    this.watched = watched;
+    this.stars = stars;
 
+    function isWatched() {}
 
+    function isName() {}
+
+    function whatGenre() {}
+
+    function howManyStars() {}
+  }
+}
