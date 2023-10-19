ef52cea3f3fdfbee52a6f7797c26618d81daaabb
Blaine Curren <blaine.curren@gmail.com>
2023-10-18 21:02:29 -0500

    finished card game

--- class27-materials/card-game/js/main.js
+++ class27-materials/card-game/js/main.js
@@ -13,15 +13,40 @@ fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 document.querySelector("button").addEventListener("click", drawTwo);
 
 function drawTwo() {
+  // TODO: ↓ this api fetch is not working ↓
   const url = `https://www.decksofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
 
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      deckId = data.deck_id;
+      document.querySelector("#player1").src = data.cards[0].image;
+      document.querySelector("#player2").src = data.cards[1].image;
+      let player1Val = Number(data.cards[0].value);
+      let player2Val = Number(data.cards[1].value);
+      if (player1Val > player2Val) {
+        document.querySelector("h3").innerText = "Player 1 Wins";
+      } else if (player1Val < player2Val) {
+        document.querySelector("h3").innerText = "Player 2 Wins";
+      } else {
+        document.querySelector("h3").innerText = "Time for War!";
+      }
     })
     .catch((err) => {
       console.log(`error${err}`);
     });
+}
+
+function convertToNum() {
+  if (val === "ACE") {
+    return 14;
+  } else if (val === "KING") {
+    return 13;
+  } else if (val === "QUEEN") {
+    return 12;
+  } else if (val === "JACK") {
+    return 11;
+  } else {
+    return Number(val);
+  }
 }
