26333388dd904ee01dbe2c3f7085fa581cfa83f6
Blaine Curren <78461346+blainecurren@users.noreply.github.com>
2023-10-23 20:17:38 -0500

    updated dnd

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -8,7 +8,16 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+       console.log(data.subclasses)
+       data.subclasses.forEach(obj => {
+       console.log(obj.name)
+       // create an li var to append
+       const li = document.createElement('li')
+       // add text to li
+       li.textContent = obj.name
+       // append the li to the ul
+       document.querySelector('ul').appendChild(li)
+       })
       })
       .catch(err => {
           console.log(`error ${err}`)
