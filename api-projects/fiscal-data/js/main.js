080f2267eef4eb549b6ab143f8be68f65a8314a1
Blaine Curren <blaine.curren@gmail.com>
2023-10-27 01:52:53 -0500

    cash compass may not be viable

--- api-projects/fiscal-data/js/main.js
+++ api-projects/fiscal-data/js/main.js
@@ -4,14 +4,14 @@ document.querySelector("button").addEventListener("click", getFetch);
 function getFetch() {
   const choice = document.querySelector("input").value;
   console.log(choice);
-  const url = ``;
+  const url =
+    (`https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/rates_of_exchange${choice}`,
+    { mode: "no-cors" });
 
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
-
-      document.querySelector("h3").innerText = data.explanation;
     })
     .catch((err) => {
       console.log(`error ${err}`);
