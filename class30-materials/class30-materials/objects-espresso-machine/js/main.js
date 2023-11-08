38d4d779bcf9a4a55f20ee2a51008b5c61e77613
Blaine Curren <blaine.curren@gmail.com>
2023-11-08 12:02:50 -0600

    completed espressoMachine

--- class30-materials/class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,21 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+  constructor(color, make, model, price) {
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.price = price;
+  }
+  turnOn() {
+    alert(`You got ${this.size}`);
+  }
+  steam() {
+    alert(`You got ${this.brand}`);
+  }
+  brew() {
+    alert(`You got ${this.price}`);
+  }
+}
+
+let gaggia = new EspressoMachine("red", "gaggia", "classic pro", 400);
