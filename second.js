2596e99144e4998214f02ee6bf72a3a09142dda9
Blaine Curren <blaine.curren@gmail.com>
2023-11-10 07:55:48 -0600

    added new codewars

--- second.js
+++ second.js
@@ -1,1 +1,17 @@
-
+class EspressoMachine {
+  constructor(color, make, model, price) {
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.price = price;
+  }
+  turnOn() {
+    alert("");
+  }
+  steam() {
+    alert("");
+  }
+  brew() {
+    alert("");
+  }
+}
