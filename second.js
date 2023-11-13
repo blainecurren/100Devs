2c9a5a564e699428baf0ad6fa5a93d4df885f227
Blaine Curren <blaine.curren@gmail.com>
2023-11-13 07:55:33 -0600

    added 7kyu cw

--- second.js
+++ second.js
@@ -1,17 +1,1 @@
-class EspressoMachine {
-  constructor(color, make, model, price) {
-    this.color = color;
-    this.make = make;
-    this.model = model;
-    this.price = price;
-  }
-  turnOn() {
-    alert("");
-  }
-  steam() {
-    alert("");
-  }
-  brew() {
-    alert("");
-  }
-}
+
